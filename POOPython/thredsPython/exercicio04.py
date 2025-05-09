1c849cbd4041744b3f892fd613f47bd7701ac0a2
PelehAlbino <marciofilho2004@bol.com.br>
2025-05-09 12:48:41 -0300

    update ex 4

--- POOPython/thredsPython/exercicio04.py
+++ POOPython/thredsPython/exercicio04.py
@@ -7,29 +7,31 @@ import math
 def gerar_elementos(n):
     return [random.randint(2,100) for _ in range(n)]
 
-def gerar_fator(lista):
+def gerar_fator(n):
     primo = 2
     fator = []
-    for valor in lista:
-        aux = []
-        # Usando a raiz quadrada do valor porque é o mais eficiente para descobrir os numeros primos
-        while primo <= math.sqrt(valor):
-            if valor % primo == 0:
-                fator.append(primo)
-                valor //= primo # valor = valor // primo
-            else:
-                primo += 1
-        fator.append((valor, aux))
+    while primo <= math.sqrt(n):
+        if n % primo == 0:
+            fator.append(primo)
+            n //= primo
+        else:
+            primo += 1
     return fator
 
+def fatorar(n):
+    return(n, gerar_fator(n))
+    
 
+# Função Principal
 def main():
     num_elementos = 5
     lista = gerar_elementos(num_elementos)    
     inicio = time.time()
+    print(lista)
+    
     # Execução em Paralelo
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        fator = list(executor.map(gerar_fator, lista))
+        fator = list(executor.map(fatorar, lista))
     
     fim = time.time()
     
